f9ed556a7dc2ea49850da49d9c871652bdc7d2b6
Annashya Kundu <125907589+KUNDUANNASHYA@users.noreply.github.com>
2024-06-25 21:34:31 +0530

    Add files via upload

---
+++ addtwovalues.java
@@ -0,0 +1,10 @@
+class AddtwoValues
+{
+    public static void main(string[] args)
+    {
+        int a,b,c;
+        a=10;
+        b=20;
+        System.out.println("The total value is" + c);
+    }
+}
